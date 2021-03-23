f33910fa064d810d41a499a6de28245789f5b734
Faisal C A <faisal.ca@nestgroup.net>
2021-03-23 16:49:48 +0530

    faisal334

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
   form!:FormGroup;
   errorFlag:boolean=false;
  //git testing 3
- //git testing 10-reshmi
+ //git testing 11-faisal
  
   constructor(public auth:AuthService,
               public fb:FormBuilder,
