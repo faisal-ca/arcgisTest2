9e9cdb9572e6be8e8deca4a87880bf0c60f6dc52
Praveen Roy <royichan003@gmail.com>
2021-03-24 11:44:57 +0530

    Praveen saved new

--- frontend/src/app/login/login.component.ts
+++ frontend/src/app/login/login.component.ts
@@ -12,7 +12,7 @@ export class LoginComponent implements OnInit {
   form!:FormGroup;
   errorFlag:boolean=false;
  //git testing 3
- //git testing 11-faisal
+ //git testing 11-PraveenRoy
  
   constructor(public auth:AuthService,
               public fb:FormBuilder,
