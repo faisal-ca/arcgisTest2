ccfaf04aff10bfc520f023606e5bbeb252bf168c
reshmi <reshmi697@gmail.com>
2021-03-30 10:51:33 +0530

    bookmarkedit

--- frontend/src/app/services/homeAuth.ts
+++ frontend/src/app/services/homeAuth.ts
@@ -65,6 +65,7 @@ export class HomeAuthService implements  CanActivate{
     setView(v:any){
       this.view=v;
     }
+    
     panMap(coordinates:any) {
       this.view.goTo(coordinates)
       .then(() => {
